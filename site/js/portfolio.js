797a4568d5be6216289cb5e11045f4e47bbd3c6c
Samir B <smdae99@gmail.com>
2023-12-12 09:23:09 +0100

    Added step1 javascript

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,6 @@
 document.getElementById('swap-avatar').addEventListener("click", avatarSwap);
 
     function avatarSwap() {
-      var img = document.getElementById('your-image-id'); // Replace 'your-image-id' with the actual ID of your image
+      let img = document.getElementById('swap-avatar');
       img.src = "image/avatar.svg";
       img.alt = "New alt here";}
